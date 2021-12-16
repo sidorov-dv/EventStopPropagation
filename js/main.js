70a0db8c06a3d35a0f49071e4619690c56882d32
Dmitry <demsys14@gmail.com>
2021-12-16 12:46:24 +0300

    eventEmit with/without stopPropagation

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let outDiv = document.getElementById('out');
+let inDiv = document.getElementById('in');
+
+outDiv.addEventListener('click', function(e) {
+    console.log(`Event target is #${e.target.id}; Handler is #${this.id}`)
+    outDiv.style.background = 'red'
+    setTimeout (() => {
+        outDiv.style.background = '';
+    }, 2000)
+})
+
+inDiv.addEventListener('click', function(e) {
+    console.log(`Event target is #${e.target.id}; Handler is #${this.id}`)
+    inDiv.style.background = 'blue';
+    setTimeout(() => {
+        inDiv.style.background = '';
+    }, 2000)
+})
+
+let outDivStop = document.getElementById('outStop');
+
+outDivStop.addEventListener('click', function(e) {
+    console.log(`Event target is #${e.target.id}; Handler is #${this.id}`)
+    outDivStop.style.background = 'red'
+    setTimeout (() => {
+        outDivStop.style.background = '';
+    }, 2000)
+})
+
+let inDivStop = document.getElementById('inStop');
+
+inDivStop.addEventListener('click', function(e) {
+    console.log(`Event target is #${e.target.id}; Handler is #${this.id}`)
+    inDivStop.style.background = 'blue';
+    e.stopPropagation();
+    setTimeout(() => {
+        inDivStop.style.background = '';
+    }, 2000)
+})
